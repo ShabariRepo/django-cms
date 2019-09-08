245079ec4f87731a484623287cd52a6ed75b527d
sshenoy <sshenoy@centrilogic.com>
2019-09-08 13:19:28 -0400

    - home page edit to have custom fields

--- home/models.py
+++ home/models.py
@@ -6,9 +6,24 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
+    # body = RichTextField(blank=True)
+
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('body', classname="full"),
+    # ]
+    """Home page model"""
+    templates = "home/home_page.html"
+    max_count = 1
     body = RichTextField(blank=True)
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        FieldPanel("banner_title"),
     ]
-    
+
+class Meta:
+
+    verbose_name = "Centrilogic CMS - Home Page"
+    verbose_name_plural = "Centrilogic CMS - Home Pages"
