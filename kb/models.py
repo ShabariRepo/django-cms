187b6c10a6eff4689be7e705986917f5bb6baf58
sshenoy <sshenoy@centrilogic.com>
2019-09-08 15:44:41 -0400

    - stream field editors - style the kb index page header - add stream field to kb page for better editing and kb creation

--- kb/models.py
+++ kb/models.py
@@ -4,15 +4,27 @@ from modelcluster.fields import ParentalKey
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.documents.edit_handlers import DocumentChooserPanel
-# from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.core.fields import StreamField
 from wagtail.search import index
+
+from streams import blocks
 
 
 class KbIndexPage(Page):
-    intro = RichTextField(blank=True)
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image", null=True, blank=False, on_delete=models.SET_NULL, related_name="+"
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel("banner_image")
+    ]
 
     def get_context(self, request):
         # Update context to include only published posts, ordered by reverse-chron
@@ -24,7 +36,18 @@ class KbIndexPage(Page):
 class KbPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
+    # body = RichTextField(blank=True)
+
+    # stream field for multiple body elements
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
 
     def main_image(self):
         gallery_item = self.gallery_images.first()
@@ -35,15 +58,16 @@ class KbPage(Page):
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
-        index.SearchField('body'),
+        index.SearchField('content'),
     ]
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
         InlinePanel('gallery_images', label="Gallery images"),
         InlinePanel('documents', label="Documents"),
+        # FieldPanel('body', classname="full"),
+        StreamFieldPanel("content"),
     ]
 
 # image on each blog page from the gallery
--- samplecms/settings/base.py
+++ samplecms/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'home',
     'search',
     'kb',
+    'streams',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
